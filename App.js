f025bf4dbe9953ef396ebe32bfeb16c6163fab82
Vaibhav Singh <59394309+vaibhavsingh2@users.noreply.github.com>
2020-07-03 17:02:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
+import db from './config';
+
+export default function App() {
+  return (
+    <WelcomeScreen/>
+    
+  );
+}
+
